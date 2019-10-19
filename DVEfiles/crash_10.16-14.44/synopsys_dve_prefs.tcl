a618aee18694a88bfa89122d97573a4ee81a0bef
jtrinh <johnnytrinh2010@gmail.com>
2019-10-18 17:57:40 -0700

    Add gitignore and remove unnecessary files

--- DVEfiles/crash_10.16-14.44/synopsys_dve_prefs.tcl
+++
@@ -1,6 +0,0 @@
-# DVE version M-2017.03-SP1-1_Full64
-# Preferences written on Fri Oct 12 17:14:33 2018
-gui_set_var -name {read_pref_file} -value {true}
-gui_set_pref_value -category {Globals} -key {cbug_fifo_dump_enable_proc} -value {false}
-gui_create_pref_key -category {Globals} -key {load_detail_for_funcov} -value_type {bool} -value {false}
-gui_set_var -name {read_pref_file} -value {false}
